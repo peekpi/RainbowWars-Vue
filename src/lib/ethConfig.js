9b9676ee02630056b339fff5dc9f434ac15b2760
lixp <894646171@qq.com>
2020-10-02 23:51:05 +0800

    new rules and contract address

--- src/lib/ethConfig.js
+++ src/lib/ethConfig.js
@@ -1,6 +1,6 @@
 export const contractConfig = {
     //ethBridge: '0x90774f99783f1238e1193cD7030FB3d686D0786E',
-    ethBridge: '0xc47206463c72f0a771f24222c3bd29dd4fba564b',
+    ethBridge: '0x2796cAaDC53f5d907332a2573F20DF23eA57C687',
     ethNodeUrl: 'https://rinkeby.infura.io/v3/<INFURA-ID>',
     //ethNodeUrl: 'http://127.0.0.1:8545',
     ethGasLimit: 4712388,
